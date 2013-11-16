fcea515ac24af3b578808b9e47e60e662cd29afd
Scott Ballantyne <ussballantyne@gmail.com>
2013-11-15 22:47:19 -0600

    adding some aliases

--- lib/wemo.rb
+++ lib/wemo.rb
@@ -3,4 +3,12 @@ require 'wemo/switch'
 
 module Wemo
   NAMESPACE = 'http://www.belkin.com/'
+
+  def self.on(name)
+    Wemo::Switch.send_command(name, 'on')
+  end
+
+  def self.off(name)
+    Wemo::Switch.send_command(name, 'off')
+  end
 end
--- lib/wemo/switch.rb
+++ lib/wemo/switch.rb
@@ -4,6 +4,31 @@ require 'curb'
 require 'simple_upnp'
 module Wemo
   class Switch
+    def self.on(name)
+      send_command(name, 'on')
+    end
+
+    def self.off(name)
+      send_command(name, 'off')
+    end
+
+    def self.state(name)
+      send_command(name, 'off')
+    end
+
+    def self.set_binary_state(device_location, signal)
+      c = Curl::Easy.new(device_location.to_s + 'upnp/control/basicevent1')
+      c.headers["Content-type"] = 'text/xml; charset="utf-8"'
+      c.headers["SOAPACTION"] = "\"urn:Belkin:service:basicevent:1#SetBinaryState\""
+      c.verbose = false
+      begin
+        c.http_post("<?xml version='1.0' encoding='utf-8'?><s:Envelope xmlns:s='http://schemas.xmlsoap.org/soap/envelope/' s:encodingStyle='http://schemas.xmlsoap.org/soap/encoding/'><s:Body><u:SetBinaryState xmlns:u='urn:Belkin:service:basicevent:1'><BinaryState>#{signal}</BinaryState></u:SetBinaryState></s:Body></s:Envelope>")
+        c.perform
+      rescue Curb::Err
+      end
+        # c.body_str
+    end
+
     def self.send_command(wemo, action)
       case action
       when "on"
@@ -29,7 +54,6 @@ module Wemo
           if device_json['root']['device']
             if device_json['root']['device']['friendlyName']
               friendlyName = device_json['root']['device']['friendlyName']
-              # puts "WeMo Friendly Name: " + friendlyName
               if friendlyName.downcase == wemo.strip.downcase
                 wemo_device = device_json['root']['device']
                 device_name = friendlyName
@@ -41,16 +65,7 @@ module Wemo
         end
       end
       if wemo_device
-        c = Curl::Easy.new(device_location.to_s + 'upnp/control/basicevent1')
-        c.headers["Content-type"] = 'text/xml; charset="utf-8"'
-        c.headers["SOAPACTION"] = "\"urn:Belkin:service:basicevent:1#SetBinaryState\""
-        c.verbose = false
-        begin
-          c.http_post("<?xml version='1.0' encoding='utf-8'?><s:Envelope xmlns:s='http://schemas.xmlsoap.org/soap/envelope/' s:encodingStyle='http://schemas.xmlsoap.org/soap/encoding/'><s:Body><u:SetBinaryState xmlns:u='urn:Belkin:service:basicevent:1'><BinaryState>#{signal}</BinaryState></u:SetBinaryState></s:Body></s:Envelope>")
-          c.perform
-        rescue Curb::Err
-        end
-        # c.body_str
+        set_binary_state(device_location, signal)
       end
     end
   end
